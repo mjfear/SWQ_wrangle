f4d54ce44829dc827ac2defaeff5fdc4ee3207a5
Matt Fear - MDC <matt.fear@gmail.com>
2019-01-11 13:33:04 +1300

    Forgot to save before last commit - resave and recommit.

--- SWQ_wrangle.R
+++ SWQ_wrangle.R
@@ -14,24 +14,23 @@ endpointMdc <- "http://hydro.marlborough.govt.nz/mdc.hts?" ##Check this is corre
 
 sites <- getHilltopSites(endpoint = endpointMdcSwq)
 
-# measurementsFromCollection <- GetCollection()
-
 measurementsBySite <- tibble()
 for(i in 1:length(sites[,1])){
   measurements <- getHilltopMeasurements(endpoint = endpointMdcSwq, site = sites[i,1]) #change to main
   measurementNames <- unique(measurements$datasource)
-  print(paste0(length(measurementNames), " measurements at ", sites[i,1]))
-  # measurements <- bind_rows(sites[site,], measurements)
-  # measurementsBySite <- bind_rows(measurementsBySite,measurements)
+  measurements <- tibble(siteName=sites[i,1], measurementNames)
+  measurementsBySite <- bind_rows(measurementsBySite,measurements)
 }
 
-View(measurementsBySite)
+
 
 # measurements <- getHilltopMeasurements(endpoint = endpointMDC, site = sites[1,1])
-
+# 
+# siteGrabInfo <- measurementsBySite %>% 
+#   filter()
 # dataWQ <- fullGetHilltopData(endpoint = endpointMDC,
-                           sites = sites[,1],
-                           measurements = c(measurements[1,1], measurements[10,1]),
-                           from = "1/1/07",
-                           to = "1/1/19",
-                           option = "WQ") 
+#                            sites = sites[,1],
+#                            measurements = c(measurements[1,1], measurements[10,1]),
+#                            from = "1/1/07",
+#                            to = "1/1/19",
+#                            option = "WQ") 
