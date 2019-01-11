d189136b79d0ea4cb9ee850fbf87be37f231734c
Matt Fear - MDC <matt.fear@gmail.com>
2019-01-11 13:32:08 +1300

    Write section to query sites list from SWQ only file and use on full MDC hilltop file later on - this will need to be improved before further use.

--- SWQ_wrangle.R
+++ SWQ_wrangle.R
@@ -1,21 +1,37 @@
 ### prereqs ###
 
 library(tidyverse)
+library(Hilltop)
 library(hillr)
 
 ### dataset specific ###
 
-endpointMDC <- "http://hydro.marlborough.govt.nz/soewq.hts?"
+endpointMdcSwq <- "http://hydro.marlborough.govt.nz/soewq.hts?" 
+endpointMdc <- "http://hydro.marlborough.govt.nz/mdc.hts?" ##Check this is correct
 
-### Get data ###
 
-sites <- getHilltopSites(endpoint = endpointMDC)
+### Get data ### HACK to get SWQ sites list from clean dataset
 
-measurements <- getHilltopMeasurements(endpoint = endpointMDC, site = sites[1,1])
+sites <- getHilltopSites(endpoint = endpointMdcSwq)
 
-data <- fullGetHilltopData(endpoint = endpointMDC,
+# measurementsFromCollection <- GetCollection()
+
+measurementsBySite <- tibble()
+for(i in 1:length(sites[,1])){
+  measurements <- getHilltopMeasurements(endpoint = endpointMdcSwq, site = sites[i,1]) #change to main
+  measurementNames <- unique(measurements$datasource)
+  print(paste0(length(measurementNames), " measurements at ", sites[i,1]))
+  # measurements <- bind_rows(sites[site,], measurements)
+  # measurementsBySite <- bind_rows(measurementsBySite,measurements)
+}
+
+View(measurementsBySite)
+
+# measurements <- getHilltopMeasurements(endpoint = endpointMDC, site = sites[1,1])
+
+# dataWQ <- fullGetHilltopData(endpoint = endpointMDC,
                            sites = sites[,1],
-                           measurements = c("Ammonia - Nitrogen"),
-                           from = "1/1/13",
-                           to = "1/1/14",
-                           option = "WQ")
+                           measurements = c(measurements[1,1], measurements[10,1]),
+                           from = "1/1/07",
+                           to = "1/1/19",
+                           option = "WQ") 
