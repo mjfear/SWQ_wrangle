131364aef0df16198100b8ccd06a8bf0c761fdb2
Matt Fear - MDC <matt.fear@gmail.com>
2019-01-14 16:46:10 +1300

    Tidyr coding to query measurementBySite.
    Include initial wrangling and testing for data entered at non-identical times and at time spacing of less than one day.

--- SWQ_wrangle.R
+++ SWQ_wrangle.R
@@ -10,27 +10,55 @@ endpointMdcSwq <- "http://hydro.marlborough.govt.nz/soewq.hts?"
 endpointMdc <- "http://hydro.marlborough.govt.nz/mdc.hts?" ##Check this is correct
 
 
-### Get data ### HACK to get SWQ sites list from clean dataset
+### Get data ### 
 
-sites <- getHilltopSites(endpoint = endpointMdcSwq)
+sites <- getHilltopSites(endpoint = endpointMdcSwq) ## HACK to get SWQ sites list from clean dataset - replace this with a collection query or ideally a project code query if possible.
 
-measurementsBySite <- tibble()
-for(i in 1:length(sites[,1])){
-  measurements <- getHilltopMeasurements(endpoint = endpointMdcSwq, site = sites[i,1]) #change to main
-  measurementNames <- unique(measurements$datasource)
-  measurements <- tibble(siteName=sites[i,1], measurementNames)
-  measurementsBySite <- bind_rows(measurementsBySite,measurements)
+measurementsBySite <- tibble(siteName=sites[,1]) %>%
+  mutate(measurementsData=map(.x=siteName, .f=getHilltopMeasurements, endpoint = endpointMdcSwq)) 
+  
+commonMeasurementString <- measurementsBySite$measurementsData %>% 
+  map(select, datasource) %>% 
+  reduce(intersect) %>% 
+  pull ## Find the common datasource names between all sites listed in sites, so that common measurements for sites list can be queried by fullGetHilltopData
+
+  
+
+
+#Having built site list and common measurements list, query this subset from Hilltop.
+data_WQ <- fullGetHilltopData(endpoint = endpointMdcSwq,
+                             sites = measurementsBySite$siteName,
+                             measurements = commonMeasurementString,
+                             from = "1/1/1907",
+                             to = "1/1/19",
+                             option = "WQ")
+
+### Wrangle ### 
+## wrangle the dataWQ
+
+data_tidy <- data_WQ %>% 
+  select(Site, Time, Measurement, Value, `Sample ID.y`) %>% 
+  spread(Measurement, Value) %>% 
+  arrange(Time) %>% 
+  group_by(Site) %>% 
+  nest
+
+## test the data
+testTimeStamp <- function(data){
+  Time <- data$Time
+  errorsList <- if_else(as.numeric(Time-lag(Time))<1,1,0)
+  if(sum(errorsList, na.rm=TRUE)==0){
+    return(list(errorCount=as.integer(0),errorText="No errors"))}
+  else{
+    errorInfo <- which(errorsList)
+    errorString <- paste0("Check entries circa ", Time[errorInfo])
+    return(errorCount=sum(errorsList, na.rm = TRUE), errorText=errorString)
+  }
 }
 
+# testTimeStamp(dataWQ_tidy$data[[1]]$Time)
+  
+dataWQ_results <- dataWQ_tidy %>% 
+  mutate(errorAlert=map(data, testTimeStamp))
 
 
-# measurements <- getHilltopMeasurements(endpoint = endpointMDC, site = sites[1,1])
-# 
-# siteGrabInfo <- measurementsBySite %>% 
-#   filter()
-# dataWQ <- fullGetHilltopData(endpoint = endpointMDC,
-#                            sites = sites[,1],
-#                            measurements = c(measurements[1,1], measurements[10,1]),
-#                            from = "1/1/07",
-#                            to = "1/1/19",
-#                            option = "WQ") 
