41ca2ab5515825cbf89657f03b7727a5992876f8
Matt Fear - MDC <matt.fear@gmail.com>
2019-01-15 14:28:33 +1300

    Quasi commit ready to clone for Hilltop log specific records.

--- SWQ_wrangle.R
+++ SWQ_wrangle.R
@@ -12,26 +12,36 @@ endpointMdc <- "http://hydro.marlborough.govt.nz/mdc.hts?" ##Check this is corre
 
 ### Get data ### 
 
+sites <- getHilltopSites(endpoint = endpointMdc)
+sites <- tibble(site=c("Opawa River at Elizabeth St"))
 sites <- getHilltopSites(endpoint = endpointMdcSwq) ## HACK to get SWQ sites list from clean dataset - replace this with a collection query or ideally a project code query if possible.
 
 measurementsBySite <- tibble(siteName=sites[,1]) %>%
-  mutate(measurementsData=map(.x=siteName, .f=getHilltopMeasurements, endpoint = endpointMdcSwq)) 
+  mutate(measurementsData=map(.x=siteName, .f=getHilltopMeasurements, endpoint = endpointMdc)) 
+
+measurementsBySite_elizSt <- tibble(siteName=pull(sites[,1])) %>%
+  mutate(measurementsData=map(.x=siteName, .f=getHilltopMeasurements, endpoint = endpointMdc)) 
   
 commonMeasurementString <- measurementsBySite$measurementsData %>% 
   map(select, datasource) %>% 
   reduce(intersect) %>% 
   pull ## Find the common datasource names between all sites listed in sites, so that common measurements for sites list can be queried by fullGetHilltopData
 
-  
-
 
 #Having built site list and common measurements list, query this subset from Hilltop.
-data_WQ <- fullGetHilltopData(endpoint = endpointMdcSwq,
-                             sites = measurementsBySite$siteName,
-                             measurements = commonMeasurementString,
+data_WQ <- fullGetHilltopData(endpoint = endpointMdc,
+                             sites = sites$site,#measurementsBySite$siteName,
+                             measurements = "pH",#commonMeasurementString,
                              from = "1/1/1907",
                              to = "1/1/19",
-                             option = "WQ")
+                             option = "None")
+
+dataTest <- fullGetHilltopData(endpoint = endpointMdc,
+                           site = "Opawa River at Elizabeth St",
+                           measurement = "Conductivity (Field)", #pH
+                           from = "1/1/1907",
+                           to = "1/1/19",
+                           option = "None")
 
 ### Wrangle ### 
 ## wrangle the dataWQ
@@ -43,10 +53,16 @@ data_tidy <- data_WQ %>%
   group_by(Site) %>% 
   nest
 
+dataTest_tidy <- dataTest %>% 
+  arrange(Time) %>% 
+  group_by(Site) %>% 
+  nest
+
+
 ## test the data
 testTimeStamp <- function(data){
   Time <- data$Time
-  errorsList <- if_else(as.numeric(Time-lag(Time))<1,1,0)
+  errorsList <- if_else(as.numeric(Time-lag(Time))<3600*24,TRUE,FALSE)
   if(sum(errorsList, na.rm=TRUE)==0){
     return(list(errorCount=as.integer(0),errorText="No errors"))}
   else{
@@ -58,7 +74,8 @@ testTimeStamp <- function(data){
 
 # testTimeStamp(dataWQ_tidy$data[[1]]$Time)
   
-dataWQ_results <- dataWQ_tidy %>% 
+data_results <- dataTest %>% 
   mutate(errorAlert=map(data, testTimeStamp))
 
+testTimeStamp(dataTest)
 
