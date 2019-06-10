941020c68d8d59f15b1a4b868a27161f1b9f2e4d
Matt Fear - MDC <matt.fear@gmail.com>
2019-06-11 11:04:07 +1200

    Commit working code prior to restructure into runMe etc.

--- SWQ_wrangle_reporting.R
+++ SWQ_wrangle_reporting.R
@@ -1,22 +1,33 @@
 # Prereqs and definitions -------------------------------------------------
 library(Hmisc)
 library(lubridate)
-library(Hilltop)
+# library(Hilltop)
 library(hillr)
 library(tidyverse)
 
+yearReport <- 2016 #set to year after 3 year cycle - e.g. yearReport <- 2016 collates 2013-2015 data for WQI calc
+
+quality_code_acceptable_minimum <- 0 #set to lowest level of acceptable data
+
 datestr <- stamp("24/02/1985")
+yearstr <- stamp_date("2009")
+
+fulldatestr <- stamp("24/02/1985 06:48:59")
 
 endpoint <- "http://hydro.marlborough.govt.nz/soewq.hts?"
-# endpoint <- "http://hydro.marlborough.govt.nz/mdc.hts?"
+### endpoint <- "http://hydro.marlborough.govt.nz/mdc.hts?"
 
 source("funcs_SWQ.R")
 
-yearReport <- 2016
 yearNow <- today() %>% year
 
-runReports <- TRUE
-param_order <- c("Turbidity", "SINitrogen", "Phosphorus", "Ecoli", "pH", "Oxygen", "Temperature", "Ammonia", "Nitrate")
+param_order <- c("Turbidity", "SINitrogen", "Phosphorus", "Ecoli", 
+                 "pH", "Oxygen", "Temperature", "Ammonia", "Nitrate")
+
+reportedWQI <- read_csv("data/reportedWQIs.csv", col_names = TRUE) %>%
+  gather(key = yearStart, value = reportedWQI, "2007":"2013") %>%
+  rename(sitePublicName = publicName) %>%
+  mutate(yearStart = yearStart %>% as.double())
 
 # Wrangle data ------------------------------------------
 
@@ -27,8 +38,11 @@ sites <- sites_list %>%
   left_join(getHilltopSites(endpoint), by = c("hilltopName" = "site"))
   
 parameters_list <- read_csv("data/parameterText.csv") %>% 
-  mutate(parameterUnits = str_replace_na(parameterUnits, replacement = "")) 
-  
+  mutate(parameterUnits = str_replace_na(parameterUnits, replacement = ""),
+         parameterLabel = str_c(parameterPublicName, 
+                                if_else(parameterUnits == "", 
+                                        "", 
+                                        str_c(" [", parameterUnits, "]")))) 
 
 measurement_query <-  parameters_list %>% 
   select(hilltopMeasurementName) %>% filter(is.na(.) == FALSE) %>% pull
@@ -46,27 +60,39 @@ measurements_raw <- sites_list %>%
   unnest() %>% ungroup %>% select(-hilltopName)
 
 measurements <- measurements_raw %>% 
-  select(sitePublicName:Time, Measurement, Value, QualityCode, QAComment = `QA Comment`, Project, SampleID = `Sample ID`) %>% 
+  select(sitePublicName:Time, Measurement, Value, QualityCode, 
+         QAComment = `QA Comment`, Project, SampleID = `Sample ID`) %>% 
   mutate(Value = Value %>% str_remove(">|<") %>% as.double(),
          QualityCode = QualityCode %>% as.integer()) %>% 
-  left_join(parameters_list %>% select(hilltopParameterName, parameter = shortParameterName), 
+  left_join(parameters_list %>% 
+              select(hilltopParameterName, 
+                     parameter = shortParameterName), 
               by = c("Measurement" = "hilltopParameterName")) %>% 
   select(-Measurement) %>% 
   filter(Project == "SW SoE" | is.na(Project) == TRUE) 
 
 measurements_spread <- measurements %>% 
-  filter(QualityCode >= 500) %>% 
+  # filter(QualityCode >= quality_code_acceptable_minimum | is.na(QualityCode) == TRUE) %>% #invoke project rules
   select(-QualityCode, -QAComment) %>% 
   spread(parameter, Value) %>%
   mutate(SINitrogen = Ammonia + Nitrite) %>% 
   select(-Nitrite) %>% 
-  arrange(Site, Time)
+  arrange(Site, Time) 
 
-measurements_long <- measurements_spread %>% 
+removeSampleNo <- c(20141098) #Kaituna spurious March 2014
+
+measurements_spread_filtered <- measurements_spread %>% 
+  filter(!SampleID %in% removeSampleNo)
+
+measurements_long <- measurements_spread_filtered %>% 
   gather(param, value, Ammonia:SINitrogen) %>% 
-  arrange(Site, Time)
+  arrange(Site, Time) %>% 
+  left_join(parameters_list %>% select(shortParameterName, parameterLabel), c("param" = "shortParameterName"))
 
-measurements_WQI_long <- measurements_spread %>%
+filterSites <- list("Lower Waihopai", 2007, 2009)
+
+measurements_WQI_long <- measurements_spread_filtered %>% 
+  filter(!SampleID %in% removeSampleNo) %>%
   group_by(sitePublicName, reportRegion, Site) %>% 
   nest %>% 
   mutate(WQI_calcs = map(data, func_SWQ_calcWQI, guidelines_tbl)) %>% 
@@ -74,32 +100,104 @@ measurements_WQI_long <- measurements_spread %>%
   spread(parameter, F2F3) %>% 
   mutate(param_contrib = 100 - WQI) %>% 
   gather(param, value, WQI:Nitrate) %>% 
-  mutate(isnotWQI = if_else(param != "WQI", "Parameter Contribution", "WQI") %>% factor(levels = c("WQI", "Parameter Contribution")))
+  mutate(isnotWQI = if_else(param != "WQI", "Parameter Contribution", "WQI") %>% factor(levels = c("WQI", "Parameter Contribution"))) %>% 
+  arrange(Site, yearStart) %>% 
+  left_join(parameters_list %>% select(shortParameterName, parameterLabel), c("param" = "shortParameterName"))
 
-# measurements_WQI_wide <- measurements_WQI_long %>%
-#   spread(parameter, F2F3) %>% 
-#   gather(key, value, WQI:Nitrate) %>% 
-#   spread(key, value)
-# %>%  unnest(.sep = "_")
+
+# Inspect missing WQI - disposable section --------------------------------
+# measurements_long %>% 
+#   filter(reportRegion %in% c("Lower Wairau***", "Upper and Mid Wairau") &
+#            sitePublicName %in% c("Upper Wairau", "Lower Wairau")) %>% 
+#   ggplot(aes(x = Time, y = value, colour = param)) + 
+#     geom_point() + 
+#     geom_line() +
+#     facet_grid(param ~ Site, scales = "free_y")
+# 
+# 
+# measurements_WQI_long %>% 
+#   filter(reportRegion %in% c("Lower Wairau", "Upper and Mid Wairau") &
+#            sitePublicName %in% c("Upper Wairau", "Lower Wairau")) %>% 
+#   ggplot(aes(x = yearStart, y = value, colour = sitePublicName)) + 
+#     geom_point() + 
+#     geom_line() +
+#     facet_grid(param ~ Site, scales = "free_y")
 
 # Compare data with reported values ---------------------------------------
 
 # measurements_WQI_HTlive <- measurements_WQI_wide
 # measurements_WQI_SWQClean <- measurements_WQI_wide
 # 
-# reportedWQI <- read_csv("data/reportedWQIs.csv", col_names = TRUE) %>%
-#   gather(key = yearStart, value = reportedWQI, "2007":"2013") %>%
-#   rename(sitePublicName = publicName) %>%
-#   mutate(yearStart = yearStart %>% as.double())
-# 
+
+# Hydro Data Export -------------------------------------------------------
+
+filterSites <- list("Wairau Diversion", 2013, 2015)
+
+hydroData <- read_csv("data/hydroExportSWQData.csv",
+                      col_names = c("Site", "Date", "Temperature", "Oxygen",
+                                    "Nitrate", "Nitrite", "Phosphorus",
+                                    "Ammonia", "Ecoli", "pH", "Turbidity"),
+                      skip = 2
+                      ) %>%
+  mutate_all(~replace(., is.na(.), -500)) %>%
+  mutate(Date = Date %>% dmy_hms(),
+         Index = "",
+         SINitrogen = Ammonia + Nitrite) %>%
+  left_join(sites_list, by = c("Site" = "hilltopName")) %>%
+  select(site = sitePublicName, Date, Index, Temperature, Oxygen, pH,
+         Phosphorus, Ecoli, Ammonia, SINitrogen, Turbidity, Nitrate) %>%
+  filter(site == filterSites[[1]]) %>%
+  filter(Date %>% year() %>% between(filterSites[[2]], filterSites[[3]]))
+  # write_csv(path = "output/hydroFilteredSWQData.csv", col_names = FALSE)
+
+write.table(hydroData, "clipboard", sep = "\t", row.names = FALSE, col.names = FALSE)
+
+
+  
+  
 # compare_WQI_HTlive <- measurements_WQI_HTlive %>%
-# # compare_WQI_SWQClean <- measurements_WQI_SWQClean %>%
-#   left_join(sites_list) %>%
-#   left_join(reportedWQI) %>%
-#   select(-hilltopName, -starts_with("contrib_")) %>%
-#   mutate(difference = (WQI-reportedWQI)/reportedWQI) %>%
-#   filter(abs(difference) > 0.02 & n_all_samples >= 30 * 9) %>%
-#   arrange(desc(difference))
+compare_WQI_SWQClean <- measurements_WQI_long %>%
+  select(-isnotWQI, -parameterLabel) %>% 
+  spread(key = "param", value = "value") %>% 
+  left_join(sites_list) %>%
+  left_join(reportedWQI) %>%
+  select(-hilltopName, -starts_with("contrib_")) %>%
+  mutate(difference = abs(WQI-reportedWQI) %>% round(2),
+         percentDifference = abs((WQI-reportedWQI)/WQI) %>% round(2)) %>%
+  # filter(abs(difference) > 0.02 & n_all_samples >= 30 * 9) %>%
+  arrange(desc(difference)) %>% 
+  select(sitePublicName:param_contrib, WQI:percentDifference, Ammonia:Turbidity) %>%
+  filter(is.na(reportedWQI) != TRUE & n_all_samples > 270) %>% 
+  select(sitePublicName, yearStart:yearEnd, WQI, reportedWQI, reportRegion:Turbidity)
+ 
+
+
+# timeWindow <- 3600*24*7 #7 days in seconds
+# 
+# checkTimes <- measurements_spread %>%
+#   group_by(sitePublicName) %>%
+#   arrange(sitePublicName, Time) %>% 
+#   mutate(timeError = lead(Time)-Time) %>% 
+#   # filter(sitePublicName == "Black Birch Stream") %>%  
+#   filter(timeError < timeWindow | lag(timeError < timeWindow))
+# 
+# checkTimes %>% mutate(Time = Time %>% fulldatestr()) %>% 
+#   write_csv("output/listPossibleTimeErrors_Steffi.csv", na = "")
+
+
+
+compare_full_clean <- measurements_spread_filtered %>% 
+  filter(sitePublicName == filterSites[[1]] & 
+           year(Time) %>% between(filterSites[[2]], filterSites[[3]]))
+
+write_csv(compare_WQI_SWQClean, "output/SWQClean_compareWQI_Steffi_ranked.csv")
+write_csv(compare_full_clean %>% 
+            mutate(IndexPeriod = NA) %>% 
+            select(sitePublicName,Time, IndexPeriod, Temperature, Oxygen,
+                   pH, Phosphorus, Ecoli,Ammonia, SINitrogen, Turbidity, Nitrate), 
+          "output/SWQClean_compareFull_Steffi.csv")
+
+
 
 # compare_WQI_HTlive %>%
 #   ggplot(aes(x = yearStart, y = difference, colour = sitePublicName, fill = sitePublicName)) +
@@ -113,7 +211,13 @@ measurements_WQI_long <- measurements_spread %>%
 # Data visualisation ------------------------------------------------------
 
 reportTheme <- theme_grey()
-WQI_boundaries <- geom_hline(yintercept = c(45,65,80,95), colour = c("orange","gold","green", "lightblue"), size = c(1, 1, 1, 0.5))
+WQI_boundaries <- data.frame(cat = c("Poor", "Marginal", "Fair", "Good",  "Excellent"),
+                             cat_low = c(0, 45, 65, 80, 95),
+                             cat_high = c(45, 65, 80, 95, 100)
+                             # cat_colour = c("red", "orange","gold","green", "lightblue")
+                             )
+  
+# WQI_boundaries <- geom_hline(yintercept = c(45,65,80,95), colour = c("orange","gold","green", "lightblue"), size = c(1, 1, 1, 0.5))
 
 param_colours <- parameters_list %>% select(parameterColours) %>% filter(parameterColours %>% is.na == F) %>% pull
 param_names <- parameters_list %>% select(shortParameterName) %>% filter(shortParameterName != "Nitrite") %>% pull
@@ -124,18 +228,22 @@ param_colour_scale <- scale_fill_manual(drop = FALSE, values = param_colours)
 # All sites WQI
 measurements_WQI_long %>% 
   group_by(sitePublicName, yearStart) %>% 
-  filter(yearEnd == yearReport) %>% 
-  ggplot(aes(x = reorder(sitePublicName, desc(param_contrib)), y = abs(value), fill = param)) + #add/remove abs to vary plot
+  filter(yearEnd == yearReport-1) %>% 
+  ggplot(aes(x = reorder(sitePublicName, desc(param_contrib)), y = (value), fill = param)) + #add/remove abs to vary plot
     coord_flip() +
     geom_col(width = 0.5) +
-    # geom_col(aes(y = (WQI)/9), fill = "blue", width = 0.5) +
-    WQI_boundaries +
+    geom_hline(data = WQI_boundaries[2:5,], #aes(yintercept = cat_low, colour = cat), 
+               aes(yintercept = cat_low, colour = cat), 
+               size = c(1,1,1,0.4)
+               # colour = c("Marginal" = "orange", "Fair" = "gold", "Good" = "green", "Excellent" = "lightblue")
+               ) +
     scale_fill_manual(drop = FALSE, breaks = c("WQI", names(param_colours)), 
                       values = c(WQI = "blue", param_colours)) + 
     scale_y_continuous(breaks = seq(-100, 100, 10), position = "bottom",
-                       sec.axis = sec_axis(~.-100, breaks = seq(-100, 0, 10), name = "Parameter subtraction from WQI")) +
+                       sec.axis = sec_axis(~.-100, breaks = seq(-100, 0, 10), 
+                                           name = "Parameter subtraction from WQI")) +
     labs(y = paste0("Water Quality Index "), x = NULL) +
-    theme(legend.title = element_blank())
+    theme(legend.title = element_blank()) 
 
 # All sites WQI - facet grid
 measurements_WQI_long %>% 
@@ -153,31 +261,80 @@ measurements_WQI_long %>%
                                isnotWQI = factor("WQI", levels = c("WQI", "Parameter Contribution"))), 
              aes(yintercept = yin), colour = c("orange","gold","green", "lightblue"), size = c(1, 1, 1, 0.5)) 
 
-
-
-
-# # Select region WQI
-boxPlot_params <- param_order[1:6]
-guidelines_low <- guidelines_tbl$guideline_lower %>% set_names(nm = guidelines_tbl$shortParameterName)
-# guidelines_low                                                                                                                                                                 [guidelines_low < 0.01] <- -Inf
-guidelines_high <- guidelines_tbl$guideline_upper %>% set_names(nm = guidelines_tbl$shortParameterName)
-
-measurements_long %>%
-  filter(year(Time) >= yearReport - 3 & year(Time) <= yearReport - 1) %>% 
-  filter(reportRegion == "South Marlborough" & param %in% boxPlot_params) %>%
-  ggplot(aes(x = sitePublicName, y = abs(value), fill = param, color = param)) +
-  stat_summary(geom = "linerange", fun.data = median_hilow, fun.args = list(conf.int = 1), size = 0.75) +
-  geom_boxplot(outlier.shape = NA, alpha = 0.2, size = 0.75) +
-  geom_hline(data = data.frame(yin = guidelines_high, param = names(guidelines_high)), aes(yintercept = yin, col = param)) + 
-  coord_flip() +
-  scale_color_manual(name = "Measured Parameter", breaks = c(names(param_colours)), 
-                     values = c(param_colours), aesthetics = c("colour", "fill")) +
-  # scale_fill_manual(name = "Measured Parameter", breaks = c(names(param_colours)), values = c(param_colours)) + 
-  theme(axis.title = element_blank()) +
-  facet_wrap(. ~ param, scales = "free_x", nrow = 3) 
+# 
+# # # Select region WQI
+# boxPlot_params <- param_order#[1:6]
+# 
+# guidelines_low <- guidelines_tbl$guideline_lower %>% set_names(nm = guidelines_tbl$shortParameterName)
+# guidelines_low[guidelines_low < 0.01] <- NA
+# guidelines_high <- guidelines_tbl$guideline_upper %>% set_names(nm = guidelines_tbl$shortParameterName)
+# guidelines_high["Oxygen"] <- NA
+# param_labels <- parameters_list$parameterLabel %>% set_names(nm = parameters_list$shortParameterName)
+# 
+# measurements_long %>%
+#   filter(year(Time) >= yearReport - 3 & year(Time) <= yearReport - 1) %>% 
+#   filter(reportRegion == "South Marlborough" & param %in% boxPlot_params) %>%
+#   ggplot(aes(x = sitePublicName, y = abs(value), fill = param, color = param)) +
+#   stat_summary(geom = "linerange", fun.data = median_hilow, fun.args = list(conf.int = 1), size = 0.75) +
+#   geom_boxplot(outlier.shape = NA, alpha = 0.2, size = 0.75) +
+#   geom_hline(data = data.frame(yin = guidelines_high, param = names(guidelines_high)), aes(yintercept = yin, col = param)) + 
+#   geom_hline(data = data.frame(yin = guidelines_low, param = names(guidelines_low)), aes(yintercept = yin, col = param)) + 
+#   coord_flip() +
+#   scale_color_manual(name = "Parameter", breaks = c(names(param_colours)), 
+#                      values = c(param_colours), aesthetics = c("colour", "fill")) +
+#   theme(axis.title = element_blank()) +
+#   facet_wrap(. ~ param, scales = "free_x", nrow = 3, labeller = labeller(param = param_labels)) 
+#   
+# 
+# measurements_WQI_long %>% 
+#   filter(reportRegion == "South Marlborough" & param == "WQI") %>% 
+#   mutate(tspan_start = yearStart %>%  paste0("-01-01") %>% as_date(),
+#          tspan_end = yearEnd %>% paste0("-01-01") %>% as_date(),
+#          tspan = interval(tspan_start, tspan_end)) %>% 
+#   # glimpse() %>% select(tspan) %>% pluck(1) %>% int_start() %>% class 
+#   
+# ggplot(aes(x = int_start(tspan), y = 100 - param_contrib, col = sitePublicName)) +
+#     # geom_ribbon(data = WQI_boundaries, aes(ymin = cat_low, ymax = cat_high)) +
+#     geom_point(size = 2) +
+#     geom_line(size = 1) 
+# 
+# + 
+#     scale_x_datetime(name = "", 
+#                      breaks = as_date(seq(2005, 2016, 1) %>% as.character(), "%Y"),
+#                      labels = c("2005",
+#                                 "2006",
+#                                 "2007",
+#                                 "2008",
+#                                 "2009",
+#                                 "2010",
+#                                 "2011",
+#                                 "2012",
+#                                 "2013",
+#                                 "2014",
+#                                 "2015",
+#                                 "2016")
+#                                 )
+#   
+#   
   
+  #   geom_ribbon(xmin = -Inf, xmax = Inf, ymin = 0, ymax = 40, alpha = 0.2, fill = "red")
+  # 
+  # ggplot(data = WQI_boundaries, aes(xmin = -Inf, xmax = Inf, ymin = cat_bound_low, ymax = cat_bound_high))) + 
+  #   geom_rect()
+  #     
+    
 
-
+# measurements_long %>% 
+#   ggplot(aes(x = Time, y = value, col = param, fill = param)) +
+#     geom_point() +
+#     facet_wrap(~param, scale = "free_y", ncol = 1)
+# 
+measurements_WQI_long %>%
+  filter(param == "WQI") %>%
+  ggplot(aes(x = yearEnd, y = 100 - param_contrib, colour = sitePublicName)) +
+    geom_point() +
+    geom_line() + 
+    facet_wrap(~reportRegion)
 
   # geom_col(aes(y = (WQI)/9), fill = "blue", width = 0.5) +
   # WQI_boundaries +
--- funcs_SWQ.R
+++ funcs_SWQ.R
@@ -1,4 +1,4 @@
-# Date 20190313 
+# Date 20190313 Matt Fear
 # Function defined using methods defined SOE SWQ report 2016 - CM record 16206728
 # Sec B provides rough insight into parametric contribution to total Factor (F1, F2, F3) contributions. This should later be modified to a dot product treatment to highlight parametric contributions to F2 and F3
 
@@ -7,11 +7,19 @@ func_SWQ_calcWQI <- function(data, guidelines){
   data_long <- data %>% 
     gather("parameter", "value", Ammonia:SINitrogen) %>% 
     inner_join(guidelines, by = c("parameter" = "shortParameterName")) %>% 
+    ### Lines below to account for seasonal variation in Ammonia guideline value
+    mutate(guideline_upper = if_else(parameter != "Ammonia",
+                                   guideline_upper,
+                                   if_else(Time %>% month() %>% between(4, 10),
+                                           0.76, guideline_upper))) %>%
+    ### Lines above to account for seasonal variation in Ammonia guideline value 
     mutate(fail = value < guideline_lower | value > guideline_upper,
            excursion = if_else(fail == FALSE, 0,
                                if_else(value > guideline_lower & value > guideline_upper,
                                        value/guideline_upper - 1,
                                        guideline_lower/value - 1))) %>% 
+    
+    # return(data_long)
     select(-c("guideline_lower", "guideline_upper"))
   
   year_min <- data$Time %>% min %>% year()
@@ -40,14 +48,80 @@ func_SWQ_calcWQI <- function(data, guidelines){
            n_all_samples = map(data, "n_not_na") %>% map_dbl(sum),
            F1_tot = map(data, "fail_sum") %>% map_dbl(func_F1_aggregate),
            F2_tot = map(data, "fail_sum") %>% map_dbl(sum) / n_all_samples * 100,
+           excursion_tot = map(data, "excursion_sum") %>% map_dbl(sum),
            nse = map(data, "excursion_sum") %>% map_dbl(sum) / n_all_samples,
            F3_tot = nse / (0.01 * nse + 0.01),
-           WQI = 100 - sqrt(F1_tot ** 2 + F2_tot ** 2 + F3_tot ** 2)/1.732) %>% 
-    #### Section B:       
+           WQI = 100 - sqrt(F1_tot ** 2 + F2_tot ** 2 + F3_tot ** 2)/1.732) %>%
+    #### Section B:
     unnest() %>%
-    mutate(F2 = fail_sum / n_all_samples,
-           nse_param = excursion_sum / n_all_samples,
-           F3 = nse_param / (0.01 * nse_param + 0.01) / 100) %>% 
-    select(-contains("nse"), -n_sample:-excursion_sum) 
+    mutate(F2_param = fail_sum / n_all_samples * 100,
+           F3_param = excursion_sum / excursion_tot * F3_tot,
+           F2F3 = (F2_param/F2_tot + F3_param/F3_tot) * -1 * 0.5 * (100 - WQI)) %>%
+    select(-c(excursion_tot, contains("nse"), n_sample:F3_param)) %>% 
+    mutate(parameter = factor(parameter, levels = param_order)) #include to set order of plots
+    
 
 }
+
+
+
+# Backup code -------------------------------------------------------------
+
+# 
+# 
+# # Date 20190313 Matt Fear
+# # Function defined using methods defined SOE SWQ report 2016 - CM record 16206728
+# # Sec B provides rough insight into parametric contribution to total Factor (F1, F2, F3) contributions. This should later be modified to a dot product treatment to highlight parametric contributions to F2 and F3
+# 
+# func_SWQ_calcWQI <- function(data, guidelines){
+#   #### Section A:
+#   data_long <- data %>% 
+#     gather("parameter", "value", Ammonia:SINitrogen) %>% 
+#     inner_join(guidelines, by = c("parameter" = "shortParameterName")) %>% 
+#     mutate(fail = value < guideline_lower | value > guideline_upper,
+#            excursion = if_else(fail == FALSE, 0,
+#                                if_else(value > guideline_lower & value > guideline_upper,
+#                                        value/guideline_upper - 1,
+#                                        guideline_lower/value - 1))) %>% 
+#     select(-c("guideline_lower", "guideline_upper"))
+#   
+#   year_min <- data$Time %>% min %>% year()
+#   year_max <- data$Time %>% max %>% year()
+#   
+#   data_WQI_full <- tibble(yearStart = c(year_min:year_max-2),
+#                           yearEnd = yearStart + 2,
+#                           data = list(data_long)) %>% 
+#     unnest %>% 
+#     filter(year(Time) >= yearStart & year(Time) <= yearEnd) %>% 
+#     group_by(yearStart, yearEnd, parameter) 
+#   
+#   func_F1_aggregate <- function(vector){
+#     boo <- if_else(vector > 0, 1, 0)
+#     boo_summary <- sum(boo) / length(vector) * 100
+#   }
+#   
+#   data_WQI_aggregate <- data_WQI_full %>% 
+#     summarise(n_sample = n(),
+#               n_not_na = n_sample - sum(is.na(value)),
+#               fail_sum = sum(fail, na.rm = TRUE),
+#               excursion_sum = sum(excursion, na.rm = TRUE)) %>% 
+#     ungroup() %>%
+#     nest(parameter:excursion_sum) %>%
+#     mutate(n_group = map(data, "n_sample") %>% map_dbl(max),
+#            n_all_samples = map(data, "n_not_na") %>% map_dbl(sum),
+#            F1_tot = map(data, "fail_sum") %>% map_dbl(func_F1_aggregate),
+#            F2_tot = map(data, "fail_sum") %>% map_dbl(sum) / n_all_samples * 100,
+#            nse = map(data, "excursion_sum") %>% map_dbl(sum) / n_all_samples,
+#            F3_tot = nse / (0.01 * nse + 0.01),
+#            WQI = 100 - sqrt(F1_tot ** 2 + F2_tot ** 2 + F3_tot ** 2)/1.732) %>% 
+#     #### Section B:       
+#     unnest() %>%
+#     mutate(ratio = (100 - WQI) / (F1_tot + F2_tot + F3_tot),
+#            F1 = if_else(fail_sum > 0, 1/9, 0) / 3,
+#            F2 = fail_sum / n_all_samples / 3,
+#            nse_param = excursion_sum / n_all_samples,
+#            F3 = nse_param / (0.01 * nse_param + 0.01) / 300,
+#            param_contribution = (F1 + F2 + F3) / (100 - WQI))  %>% 
+#     select(-contains("nse"), -n_sample:-excursion_sum) 
+#   
+# }
